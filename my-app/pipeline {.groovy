26bc7ff2d841a43fbb5d3b8f292bc8e82a348adf
joel <joelstephen.j@ogspay.com>
2021-12-29 10:57:48 +0530

    changes

--- my-app/pipeline {.groovy
+++ my-app/pipeline {.groovy
@@ -5,21 +5,25 @@ pipeline {
         maven "MAVEN"
         jdk "JDK"
     }
-      stage('Build') {
-            steps {
-                dir("C:\ProgramData\Jenkins\.jenkins\workspace\New_demo\my-app\") {
+
+    stages{
+        stage('Bulid'){
+
+            steps{
+                dir("C:\ProgramData\Jenkins\.jenkins\workspace\New-demo\my-app"){
+                
                 sh 'mvn -B -DskipTests clean package'
                 }
-            
             }
+
         }
-     
+    }
     post {
-       always {
-          junit(
-        allowEmptyResults: true,
-        testResults: '*/test-reports/.xml'
-      )
-      }
-   } 
+        always {
+            junit(
+                 allowEmptyResults: true,
+                 testResults: '*/test-reports/.xml'
+            )
+        }
+    }
 }
